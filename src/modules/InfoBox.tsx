abe7175011a4a08ec31d054f7bd29f3b791b1f72
Miles Rosen <mdnrosen@hotmail.co.uk>
2025-10-21 23:34:51 +0100

    leave it there for now

--- src/modules/InfoBox.tsx
+++ src/modules/InfoBox.tsx
@@ -1,8 +1,6 @@
 const InfoBox = () => {
   return (
-    <div className="p-1 h-full aspect-square grid grid-rows-5 rounded-md   w-2/5 border-2 bg-light-blue">
-      <p>Info box</p>
-    </div>
+    <div className="p-1 h-full aspect-square grid grid-rows-5 rounded-md"></div>
   );
 };
 
